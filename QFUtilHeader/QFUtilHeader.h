553a260283398c51c0b8ea431ef52be095cb6c09
dqf <897432098@qq.com>
2018-04-27 09:16:58 +0800

    增加新的文件

---
+++ QFUtilHeader/QFUtilHeader.h
@@ -0,0 +1,59 @@
+//
+//  QFUtilHeader.h
+//  QFProj
+//
+//  Created by dqf on 2018/4/27.
+//  Copyright © 2018年 dqfStudio. All rights reserved.
+//
+
+#ifndef QFUtilHeader_h
+#define QFUtilHeader_h
+
+//Ext-Category
+#define "UIApplication+HUtil.h"
+#define "UIButton+HUtil.h"
+#define "UIColor+HUtil.h"
+#define "UIDevice+HUtil.h"
+#define "UIGestureRecognizer+HUtil.h"
+#define "UIImage+Util.h"
+#define "UILabel+HUtil.h"
+#define "UINavigationController+HUtil.h"
+#define "UIScreen+HUtil.h"
+#define "UIView+HImage.h"
+#define "UIView+HRelation.h"
+#define "UIView+HUtil.h"
+#define "UIViewController+HBackHandler.h"
+#define "UIViewController+HPrinter.h"
+#define "UIViewController+HUtil.h"
+
+//Ext-Category
+#define "UIFont+HChain.h"
+#define "UIImage+HChain.h"
+
+//Ext-Category
+#define "HDataUtil.h"
+#define "HHexUtil.h"
+#define "HSpeechUtil.h"
+#define "NSDate+HUtil.h"
+#define "NSObject+HUtil.h"
+#define "NSString+HUtil.h"
+#define "NSUserDefaults+HUtil.h"
+
+//Block-Category
+#define "NSObject+BlockSEL.h"
+
+//Chain-Category
+#define "NSString+HChain.h"
+
+//HSafeUtil
+#define "HNotiCenter.h"
+#define "NSArray+HSafeUtil.h"
+#define "NSDictionary+HSafeUtil.h"
+#define "NSObject+HSafeNotify.h"
+#define "NSObject+HSafeSEL.h"
+#define "NSObject+HSwizzleUtil.h"
+#define "NSString+HSafeUtil.h"
+#define "NSTimer+HSafeUtil.h"
+#define "UIControl+HSafeUtil.h"
+
+#endif /* QFUtilHeader_h */
